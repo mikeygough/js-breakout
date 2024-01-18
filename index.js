4c42ace8c24bab5de0cfaaedf56d20f543694d72
mikey <mikey@hey.com>
2024-01-18 14:09:44 -0600

    add gradient, game-message, colors

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
+const ballColor = '#1a1a1a';
+
+// game message
+const gameMessage = document.getElementById('gameMessage');
 
 // paddle
 const paddleHeight = 10;
@@ -14,10 +18,11 @@ const paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
+const paddleColor = '#1a1a1a';
 
 // bricks
 const brickRowCount = 5;
-const brickColumnCount = 3;
+const brickColumnCount = 4;
 const brickWidth = 75;
 const brickHeight = 20;
 const brickPadding = 10;
@@ -34,6 +39,8 @@ for (let c = 0; c < brickColumnCount; c += 1) {
 // score & lives
 let score = 0;
 let lives = 3;
+const menuColor = '#1a1a1a';
+let isPlaying = true;
 
 // functions ----------------------------------------------------
 function keyDownHandler(e) {
@@ -74,8 +81,13 @@ function collisionDetection() {
           b.status = 0;
           score += 1;
           if (score === brickRowCount * brickColumnCount) {
-            alert('YOU WIN, CONGRATS!');
-            document.location.reload();
+            gameMessage.innerHTML = 'You Win! CONGRATULATIONS! Playing again in 5 seconds...';
+            gameMessage.style.display = 'block';
+            isPlaying = false;
+            // Set a 5-second timer
+            setTimeout(() => {
+              document.location.reload();
+            }, 5000);
           }
         }
       }
@@ -86,7 +98,7 @@ function collisionDetection() {
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-  ctx.fillStyle = '#0095DD';
+  ctx.fillStyle = ballColor;
   ctx.fill();
   ctx.closePath();
 }
@@ -94,7 +106,7 @@ function drawBall() {
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-  ctx.fillStyle = '#0095DD';
+  ctx.fillStyle = paddleColor;
   ctx.fill();
   ctx.closePath();
 }
@@ -109,7 +121,8 @@ function drawBricks() {
         bricks[c][r].y = brickY;
         ctx.beginPath();
         ctx.rect(brickX, brickY, brickWidth, brickHeight);
-        ctx.fillStyle = '#0095DD';
+        const color = c % 2 === 0 ? '#8dd3c7' : '#fccde5';
+        ctx.fillStyle = color;
         ctx.fill();
         ctx.closePath();
       }
@@ -119,18 +132,34 @@ function drawBricks() {
 
 function drawScore() {
   ctx.font = '16px Arial';
-  ctx.fillStyle = '#0095DD';
+  ctx.fillStyle = menuColor;
   ctx.fillText(`Score: ${score}`, 8, 20);
 }
 
 function drawLives() {
   ctx.font = '16px Arial';
-  ctx.fillStyle = '#0095DD';
+  ctx.fillStyle = menuColor;
   ctx.fillText(`Lives: ${lives}`, canvas.width - 65, 20);
 }
 
+function drawGradient(currentCanvas) {
+  const gradient = ctx.createLinearGradient(0, 0, currentCanvas.width, currentCanvas.height);
+  // Add three color stops
+  gradient.addColorStop(0, '#fee08b');
+  gradient.addColorStop(0.5, '#ffffbf');
+  gradient.addColorStop(1, '#e6f598');
+  // Set the fill style and draw a rectangle
+  ctx.fillStyle = gradient;
+  ctx.fillRect(0, 0, currentCanvas.width, currentCanvas.height);
+}
+
 function draw() {
+  if (!isPlaying) {
+    return;
+  }
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawGradient(canvas);
   drawBricks();
   drawBall();
   drawPaddle();
@@ -149,8 +178,13 @@ function draw() {
     } else {
       lives -= 1;
       if (!lives) {
-        alert('GAME OVER');
-        document.location.reload();
+        gameMessage.innerHTML = 'You Lose! Playing again in 5 seconds...';
+        gameMessage.style.display = 'block';
+        isPlaying = false;
+        // Set a 5-second timer
+        setTimeout(() => {
+          document.location.reload();
+        }, 5000);
       } else {
         x = canvas.width / 2;
         y = canvas.height - 30;
